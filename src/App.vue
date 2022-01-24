3dd4434e0dd17e6c18bddd90d4cef6fb3d1c6224
Ekrem BAL <ekrem@pragmacraft.com>
2022-01-24 16:44:03 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,94 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row justify-content-center">
+      Points: {{points}}
+    </div>
+    <div class="row justify-content-center">
+      <transition name="bounce">
+        <Card v-if="isCardVisible" :word="card.word" :restrictedWords="card.restrictedWords"/>
+      </transition>
+    </div>
+
+    <div class="row justify-content-center fixed-bottom">
+      <div class="col"><button v-on:click="newCard(-1)" type="button" class="btn btn-danger">Wrong</button></div>
+      <div class="col"><button v-on:click="newCard(0)" type="button" class="btn btn-primary" v-bind:class="{ disabled: numSkips >= 3 }">Skip</button></div>
+      <div class="col"><button v-on:click="newCard(1)" type="button" class="btn btn-success">Correct</button></div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from './components/Card.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Card,
+  },
+  data() {
+    return {
+      points: -1,
+      card: null,
+      gameCards: [{"word": "Accredited Investor","restrictedWords":["Money","Wealthy","Business","Investor","Bank",]},{"word": "B-to-B","restrictedWords":["Business","Digital Marketing","Small Business","Email","Sales",]},{"word": "B-to-C","restrictedWords":["Business to Consumer","Company","Selling","Advertising",]},{"word": "Burn Rate","restrictedWords":["Fire","Blaze","Forest","Rate","Smoke",]},{"word": "Churn Rate","restrictedWords":["Credit","Bank","Mortgage","Income","Interest",]},{"word": "Disruptive İnnovation","restrictedWords":["Breakthrough","Think","Change","Product","Phone",]},{"word": "Exit Strategy","restrictedWords":["Business","Business Plan","Strategy","Exit","Company",]},{"word": "First Mover Advantage","restrictedWords":["Business","Company","Start","Entrepreneur","Product",]},{"word": "Intellectual Property","restrictedWords":["Copyright","Patent","Trademark","Intellectual","Lawyer",]},{"word": "Launch","restrictedWords":["Rocket","Shuttle","Blast","Lift-Off","Airplane",]},{"word": "Lean Startup","restrictedWords":["Business","Concept","Idea","Building","Startup",]},{"word": "Leverage","restrictedWords":["Cog","Machine","Gear","Force","Power",]},{"word": "Market Penetration","restrictedWords":["Strategy","Business","Product","Sales","Statistics",]},{"word": "Monetize Model","restrictedWords":["Business","Marketing","Branding","Strategy","Analytics",]},{"word": "Minimum Viable Product","restrictedWords":["Business","Product","Idea","Beta","Test",]},{"word": "Ramen Profitable","restrictedWords":["Dish","Noodles","Microwave","Packet","Instant",]},{"word": "Responsive Design","restrictedWords":["Website","Code","Responsive","Mobile","Open",]},{"word": "Return On Investment (ROI)","restrictedWords":["Profit","Company","Profit Margin","Dollar","Return",]},{"word": "SaaS","restrictedWords":["Cloud","Software","Internet","Technology","Program",]},{"word": "Traction","restrictedWords":["Snow","Ice","Car","Slippery","Skid",]},{"word": "Business Valuation","restrictedWords":["Accountant","Company","Defined","Value","Stock",]},{"word": "Value Proposition","restrictedWords":["Marketing","Advertising","Business","Customer","Offer",]},{"word": "Crowdfunding","restrictedWords":["Kickstarter","GoFundMe","Pledging","Donations","Financial",]},{"word": "Term Sheet","restrictedWords":["Contract","Financial","Investing","Borrowing","Equity",]},{"word": "Break Even Point","restrictedWords":["Accounting","Income","Profit","Loss","Expenses",]},{"word": "Preferred Stock","restrictedWords":["Investment","Company","Business","Stockholder","Shareholder",]},{"word": "Dilution","restrictedWords":["Water","Solution","Thin","Concentration","Reduce",]},{"word": "Acquisitions","restrictedWords":["Book","Buy","Sell","Trade","Shopping","Library",]},{"word": "Due Diligence","restrictedWords":["Law","Attorney","Research","Case","Case Study",]},{"word": "Initial Public Offering (IPO)","restrictedWords":["Stock","Stock Market","Company","Raise","Increase",]},{"word": "Joint Venture","restrictedWords":["Partnership","Business","Employees","Company","Goals",]},{"word": "Merger","restrictedWords":["Company","Acquire","Business","Corporate","Partnership",]},{"word": "Series A-B-C","restrictedWords":["Smallest-Largest","Numbers","Alphabet","A to Z",]},{"word": "Seed Capital","restrictedWords":["Investment","Venture","Money","Fund","Business",]},{"word": "Venture Capital","restrictedWords":["Investment","Business","Start-Up","Company","Venture",]},{"word": "Private Equity","restrictedWords":["Money","Company","Investment","Bank","Wall Street",]},{"word": "Angel investment","restrictedWords":["Business","Startup","Rich","Rich People","Sponsor"]}],
+      curCard: -1,
+      isCardVisible: true,
+      numSkips: 0,
+    }
+  },
+  methods: {
+    getRandomCard(){
+      this.curCard++;
+      return this.gameCards[this.curCard];
+    },
+    newCard(increment){
+      if(increment == 0){
+        if(this.numSkips >= 3){
+          return;
+        }
+        this.numSkips++;
+      }
+      this.points += increment;
+      // alert('newVard');
+      this.isCardVisible = false;
+      this.card = this.getRandomCard();
+      // alert(this.card);
+      setTimeout(() => {
+          this.isCardVisible = true;
+        }, 0);
+      
+    }
+
+  },
+  created() {
+    // alert('mounted');
+    this.newCard(1);
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.container{
+  margin-top: 20px;
+}
+.row{
+  margin-top: 20px;
+}
+
+.bounce-enter-active {
+  animation: bounce-in .5s;
+}
+.bounce-leave-active {
+  animation: bounce-in .5s reverse;
+}
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.1);
+  }
+  100% {
+    transform: scale(1);
+  }
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,42 @@
+<template>
+
+<div class="col-md-auto">
+  <div class="card">
+    <div class="card-header">{{word}}</div>
+    <div class="card-body">
+      <ul class="list-group list-group-flush">
+        <li v-for="restrictedWord in restrictedWords" :key="restrictedWord" class="list-group-item">{{restrictedWord}}</li>
+        <!-- <li class="list-group-item">Digital Marketing</li>
+        <li class="list-group-item">Small Business</li>
+        <li class="list-group-item">Email</li>
+        <li class="list-group-item">Sales</li> -->
+      </ul>
+    </div> 
+  </div>
+</div>
+
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+    word: String,
+    restrictedWords: Array
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.list-group{
+  text-align:center;
+}
+.card-header{
+  text-align:center;
+}
+
+
+
+
+</style>
